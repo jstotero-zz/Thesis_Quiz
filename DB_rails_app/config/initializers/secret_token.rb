d2d260483edf2d483b1c4d0d2db880f2030f7be0
Juan Sebastian Totero <asterione@gmail.com>
2011-08-04 09:21:53 +0200

    aggiunta app per il DB

---
+++ DB_rails_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DBRailsApp::Application.config.secret_token = 'cc82ddb62c06543c390d787dbf64f92d53b1b66e3f04a6e7e25e28f7e3bb5d2246f091d3b3b547f0cf23e2330a595d870704c7360e1f5361fb86d3a4e4a66657'
